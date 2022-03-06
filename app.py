99e00bbb0de3e30fef6f42ba1c15c8f8b944e903
saheedniyi02 <64325621+saheedniyi02@users.noreply.github.com>
2022-03-06 22:00:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import time
 app = Flask(__name__, template_folder="templates")
 
 
-@app.route("/<int:tweet_id>/<string: requested_word>")
+@app.route("/<int:tweet_id>/<string:requested_word>")
 def wordcloud_page(tweet_id, requested_word):
     return render_template("image.html", image_name=tweet_id, requested_word=requested_word)
 
